e6dc2c2744227fa6794addf69c98055c5707f57c
Can <eskirex@gmail.com>
2017-11-07 23:42:28 +0200

    Start

---
+++ src/BagProxy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Eskirex;
+
+/**
+ * Class BagProxy
+ *
+ * @package Eskirex
+ * @method void set($name, $value)
+ * @method array|bool get($name)
+ * @method array|bool has($name)
+ * @method array all()
+ * @method bool clear($name)
+ */
+class BagProxy extends Bag
+{
+
+}
